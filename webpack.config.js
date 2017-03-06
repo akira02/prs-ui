428cbe9a51528c698de9b6fe16a5f493b3da9b81
ray851107 <ray851107@gmail.com>
2017-03-07 02:00:03 +0800

    use awesome-typescript-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 var path = require('path');
 
 module.exports = {
-  entry: ['whatwg-fetch', './lib/main.js'],
+  entry: ['whatwg-fetch', './src/main.tsx'],
   output: {
     filename: 'bundle.js',
     path: './static'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
   }
 }
