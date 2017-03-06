99f030bb359e80f55fee5b56e6cd637fd5eab4c1
ray851107 <ray851107@gmail.com>
2017-03-06 23:48:28 +0800

    add checkbox

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react'
 import {observer, inject} from 'mobx-react'
+import autobind from 'autobind-decorator'
 
 import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
+import Checkbox from 'material-ui/Checkbox'
 
 import {Auth} from '../store/auth'
 import {Page} from './Page'
@@ -13,14 +15,21 @@ interface Props {
 
 @inject('auth') @observer
 export class LoginPage extends React.Component<Props, void> {
+    @autobind
     onSubmit (event) {
         event.preventDefault()
         const formData = new FormData(event.target)
         this.props.auth.login(formData)
     }
+    @autobind
+    onCheck (event, checked) {
+        event.preventDefault()
+        this.props.auth.remember = !this.props.auth.remember
+    }
     render () {
+        const {auth} = this.props
         return <Page>
-            <form id="login" onSubmit={this.onSubmit.bind(this)}>
+            <form id="login" onSubmit={this.onSubmit}>
                 <TextField type="text" name="username"
                     hintText="Username Field"
                     floatingLabelText="Username" />
@@ -29,6 +38,7 @@ export class LoginPage extends React.Component<Props, void> {
                     hintText="Username Field"
                     floatingLabelText="Password" />
                 <br />
+                <Checkbox label="Remember Me" checked={auth.remember} onCheck={this.onCheck}/>
                 <RaisedButton type="submit">Login</RaisedButton>
             </form>
         </Page>
