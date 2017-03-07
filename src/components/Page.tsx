25c61c87a28d3f40638d604c9a027bec494ab111
ray851107 <ray851107@gmail.com>
2017-03-07 20:28:13 +0800

    set type of props to any

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
 import Paper from 'material-ui/Paper'
 
-export const Page = (props) =>
+export const Page = (props: any) =>
     <Paper className="page" rounded={false} {...props} />
