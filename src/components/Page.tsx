49512ad92d44e348f7046a95d9596be10261fbb5
ray851107 <ray851107@gmail.com>
2017-03-08 00:23:31 +0800

    don't overwrite className

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react'
+import classNames from 'classnames'
 import Paper from 'material-ui/Paper'
 
-export const Page = (props: any) =>
-    <Paper className="page" transitionEnabled={false} rounded={false} {...props} />
+export const Page = (props: any) => {
+    const {className=null, ...rest} = props || {}
+    return <Paper
+        className={classNames('page', className)}
+        transitionEnabled={false}
+        rounded={false}
+        {...rest} />
+}
