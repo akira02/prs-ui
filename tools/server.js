c586e9fbcaa2c62ffdfaef200757d650e4ce6ea5
ray851107 <ray851107@gmail.com>
2017-03-09 10:48:52 +0800

    handle error

--- tools/server.js
+++ tools/server.js
@@ -7,10 +7,16 @@ const fileServer = new static.Server('./dist', { cache: 0 })
 const server = http.createServer((req, res) => {
     const {pathname} = url.parse(req.url)
     if (pathname.startsWith('/static')) {
-        fileServer.serve(req, res)
+        fileServer.serve(req, res).on('error', console.error)
     } else {
-        fileServer.serveFile('/index.html', 200, {}, req, res)
+        fileServer.serveFile('/index.html', 200, {}, req, res).on('error', err => {
+            console.error(err)
+            res.writeHead(404)
+            res.end()
+        })
     }
 })
+
+server.on('error', console.error)
 
 server.listen(8080)
