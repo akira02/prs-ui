5112374ef84a8e3dc3f94b5f6a8a9e20333cb85b
ray851107 <ray851107@gmail.com>
2017-03-09 11:41:19 +0800

    cool material floating button

--- src/components/AssignmentsPage.tsx
+++ src/components/AssignmentsPage.tsx
@@ -2,6 +2,9 @@ import * as React from 'react'
 import {action} from 'mobx'
 import {inject, observer} from 'mobx-react'
 
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+import ContentAdd from 'material-ui/svg-icons/content/add'
+
 import {RequireToken} from './RequireToken'
 import {Page} from './Page'
 import {AssignmentCard} from './AssignmentCard'
@@ -18,15 +21,22 @@ export class AssignmentsPage extends React.Component<Props, void> {
     handleLoggedIn () {
         this.props.assignmentList.fetch()
     }
+    @action.bound
+    handleAdd () {
+        
+    }
     render () {
         const {assignments} = this.props.assignmentList
         return <RequireToken onLoggedIn={this.handleLoggedIn}>
             <Page>
                 {
-                    assignments.map(assignment => 
+                    assignments.map(assignment =>
                         <AssignmentCard key={assignment._id} assignment={assignment}/>
                     )
                 }
+                <FloatingActionButton className="button-fixed" onTouchTap={this.handleAdd}>
+                    <ContentAdd />
+                </FloatingActionButton>
             </Page>
         </RequireToken>
     }
