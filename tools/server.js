ac3b108d29a31ed737cb2959f93bf2763191bc15
ray851107 <ray851107@gmail.com>
2017-03-12 19:25:29 +0800

    handle favicon.ico

--- tools/server.js
+++ tools/server.js
@@ -8,7 +8,10 @@ fileServer = new static.Server('./dist', { cache: 0 })
 
 const server = http.createServer((req, res) => {
     const {pathname} = url.parse(req.url)
-    if (pathname.startsWith('/static')) {
+    if (pathname === '/favicon.ico') {
+        res.statusCode = 404
+        res.end()
+    } else if (pathname.startsWith('/static')) {
         fileServer.serve(req, res).on('error', console.error)
     } else {
         fileServer.serveFile('/index.html', 200, {}, req, res).on('error', err => {
