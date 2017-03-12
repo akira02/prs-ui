edafc65f481030c66ad6e0ad9677cc6621ce589c
ray851107 <ray851107@gmail.com>
2017-03-12 17:51:28 +0800

    Revert "Revert "update mock""
    This reverts commit a3a2d795a87d06667b4dc403093ff0605c8be891.

--- test/mock.ts
+++ test/mock.ts
@@ -2,30 +2,31 @@ import fetchMock from 'fetch-mock'
 import {API_BASE} from '../src/api'
 import {Assignment} from '../src/models/Assignment'
 
+post('tokens', { token: '123123' })
+
+get('assignments', generateAssignments())
+
 function get (path, ...args) {
     return fetchMock.get('begin:' + API_BASE + path, ...args)
 }
-
 function post (path, ...args) {
     return fetchMock.post('begin:' + API_BASE + path, ...args)
 }
 
-post('tokens', {
-    token: '123123'
-})
-
-const now: number = +new Date()
-const assignments: Assignment[] = [
-    {
-        id: 'Assignment-ID-1',
-        assigned: new Date(now - 24 * 60 * 60 * 1000).toISOString(),
-        due: new Date(now + 24 * 60 * 60 * 1000).toISOString(),
-        submitted: null,
-        lesson: {
-            id: 'Lesson-ID-1',
-            name: 'Lesson1'
-        }
+function generateAssignments (): Assignment[] {
+    function date (delta): string {
+        return new Date(+new Date() + delta).toISOString()
     }
-]
-
-get('assignments', assignments)
+    return [
+        {
+            id: 'Assignment-ID-1',
+            assigned: date(- 24 * 60 * 60 * 1000),
+            due: date(+ 24 * 60 * 60 * 1000),
+            submitted: null,
+            lesson: {
+                id: 'Lesson-ID-1',
+                name: 'Lesson1'
+            }
+        }
+    ]
+}
