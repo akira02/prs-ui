c3aa1101949c86c1590b1fbe1210f20f39b322c8
ray851107 <ray851107@gmail.com>
2017-03-14 11:21:25 +0800

    minimize css in production build

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,15 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              minimize: isProduction
+            }
+          }
+        ]
       },
     ]
   },
