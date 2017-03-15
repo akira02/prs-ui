0780a650d39de0e37ef3e2b3d2f87a3ff91ef187
ray851107 <ray851107@gmail.com>
2017-03-15 10:19:51 +0800

    generate more data

--- tools/api-server/data.js
+++ tools/api-server/data.js
@@ -4,38 +4,49 @@ exports.auth = {
     TOKEN: 'MUCH-TOKEN-WOW'
 }
 
-exports.db = {
-    lessons: generateLessons(),
-    assignments: generateAssignments()
-}
+const lessons = generateLessons()
+const assignments = generateAssignments(lessons)
+
+exports.db = { lessons, assignments }
 
 function generateLessons () {
-    return [
-        {
-            id: 'Lesson-ID-1',
-            name: 'Lesson1',
+    const result = []
+    for (let i = 0; i < 10; ++i) {
+        const teacher = randomInt(5)
+        result.push({
+            id: `Lesson-ID-${i}`,
+            name: `Lesson${i}`,
             semester: '105-2',
             teacher: {
-                id: 'Teacher-ID-1',
-                name: 'Teacher1'
+                id: `Teacher-ID-${teacher}`,
+                name: `Teacher${teacher}`
             }
-        }
-    ]
+        })
+    }
+    return result
 }
 
-function generateAssignments () {
-    return [
-        {
-            id: 'Assignment-ID-1',
-            assigned: date(- 24 * 60 * 60 * 1000),
-            due: date(+ 24 * 60 * 60 * 1000),
+function generateAssignments (lessons) {
+    const ONE_MONTH = 30 * 24 * 60 * 60 * 1000
+    const result = []
+    for (let i = 0; i < 100; ++i) {
+        const lesson = lessons[randomInt(lessons.length)]
+        result.push({
+            id: `Assignment-ID-${i}`,
+            assigned: date(- randomInt(ONE_MONTH)),
+            due: date(+ randomInt(ONE_MONTH)),
             submitted: null,
             lesson: {
-                id: 'Lesson-ID-1',
-                name: 'Lesson1'
+                id: lesson.id,
+                name: lesson.name
             }
-        }
-    ]
+        })
+    }
+    return result
+}
+
+function randomInt (upper) {
+    return Math.floor(Math.random() * upper)
 }
 
 function date (delta) {
