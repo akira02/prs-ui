4a45ae14c9c0d5900c301ba007f21ebbb565b0aa
ray851107 <ray851107@gmail.com>
2017-04-14 19:42:24 +0800

    add some doc

--- src/defines.d.ts
+++ src/defines.d.ts
@@ -1,8 +1,11 @@
 // Compile time constants
 // see webpack.config.js
+
+// base url of the PRS api
 declare const API_BASE: string
-declare const process: Readonly<{
-    env: Readonly<{
-        NODE_ENV: string
-    }>
-}>
+
+declare const process: {
+    readonly env: {
+        readonly NODE_ENV: string
+    }
+}
