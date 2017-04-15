3d6dbbb1764956beb77bf1e285ed05b821f0dfcc
ray851107 <ray851107@gmail.com>
2017-04-15 11:25:41 +0800

    remove semicolon

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 const {DefinePlugin} = require('webpack')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
