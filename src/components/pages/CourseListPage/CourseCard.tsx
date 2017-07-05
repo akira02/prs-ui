c75b67ac77fe9eca52e18333e66d54b84848fa79
ray851107 <ray851107@gmail.com>
2017-07-05 19:51:10 +0800

    handle click

--- src/components/pages/CourseListPage/CourseCard.tsx
+++ src/components/pages/CourseListPage/CourseCard.tsx
@@ -1,20 +1,31 @@
 import * as React from 'react'
-import { observer } from 'mobx-react' 
+import { action } from 'mobx'
+import { inject, observer } from 'mobx-react' 
 
 import { Card, CardHeader } from 'material-ui/Card'
 
+import { History } from '../../../stores/History'
 import { CourseStore } from '../../../stores/CourseStore'
 
 export interface Props {
     store: CourseStore
+
+    // injected props
+    history?: History
 }
 
-@observer
+@inject('history') @observer
 export class CourseCard extends React.Component<Props> {
+    @action.bound
+    handleTouchTap () {
+        const {store, history} = this.props
+        history.push(`/courses/${store.course.id}`)
+    }
+
     render () {
         const {course} = this.props.store
 
-        return <Card className="card">
+        return <Card className="card" onTouchTap={this.handleTouchTap}>
             <CardHeader title={course.name} />
         </Card>
     }
