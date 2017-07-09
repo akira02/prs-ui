8e1bfd62d7140d54ced74031bde516d9b8602c92
ray851107 <ray851107@gmail.com>
2017-07-09 23:50:41 +0800

    add some docs

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
-import { autorun } from 'mobx'
-import { Provider } from 'mobx-react'
-import Router from 'universal-router'
 import { render } from 'react-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
+import { autorun } from 'mobx'
+import { Provider } from 'mobx-react'
+
+import Router from 'universal-router'
 
 import { Stores } from './stores'
 import { routes } from './routes'
@@ -12,28 +13,48 @@ import { App } from './components/App'
 
 import { api } from './api'
 
-// material-ui requires this to work properly
+// material-ui 需要這個東西
 injectTapEventPlugin()
 
-// root of all MobX stores
+// 創造所有 store
 const stores = new Stores()
 
 const router = new Router(routes, {
+    /**
+     * 將 stores 經由 context 傳給各個 route 的 action
+     */
     context: { stores },
+    
+    /**
+     * 覆寫 universal-router 預設的行為
+     */
     resolveRoute (context, params) {
         if (typeof context.route.action === 'function') {
             context.route.action(context, params)
-            // don't check action result
+
+            // 如果 route 有 action, 直接 return ture,
+            // 使 router 不再往下尋找 route
             return true
+        } else {
+
+             // return null,
+             // 使 router 繼續往下尋找 route
+            return null
         }
-        return null
     }
 })
 
+/**
+ * 當 pathname 有新的值, 自動執行 router
+ */
 autorun(() => {
     router.resolve({path: stores.history.location.pathname})
 })
 
+/**
+ * 將 stores 由 Provider 傳給各 Component
+ * 並掛到 html 裡的 .root 上
+ */
 render(
     <Provider {...stores}>
         <App />
@@ -41,6 +62,8 @@ render(
     document.getElementById('root')
 )
 
-// for debug purpose
+/**
+ * 為了 debug 方便, 把一些東西掛到 window 上
+ */
 window['stores'] = stores
 window['api'] = api
