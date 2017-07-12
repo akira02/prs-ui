37e256b2b1192b43ba8cd8a4c4dad488e66d0971
Akira <akiakira02@gmail.com>
2017-07-12 15:23:23 +0800

    add course card hover effect

--- src/components/pages/CourseListPage/index.tsx
+++ src/components/pages/CourseListPage/index.tsx
@@ -7,6 +7,7 @@ import {CourseCard} from './CourseCard'
 
 import {CourseMap} from '../../../stores/CourseMap'
 import {CourseStore} from '../../../stores/CourseStore'
+import './style.css'
 
 export interface Props {
     courseMap?: CourseMap
