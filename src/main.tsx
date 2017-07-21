d9f76cc8b553d5c6e8e6a41ec4270042f6695917
ray851107 <ray851107@gmail.com>
2017-07-21 09:21:00 +0800

    cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,21 @@
 import * as React from 'react'
-import { render } from 'react-dom'
+import {render} from 'react-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
+import {autorun} from 'mobx'
+import {Provider} from 'mobx-react'
 
-import { autorun } from 'mobx'
-import { Provider } from 'mobx-react'
+import {RootStoreModel} from './stores/RootStore'
 
+import {createRouter, runRouter} from './router'
+import {routes} from './routes'
+import {App} from './components/App'
 
-import { RootStoreModel, RootStore } from './stores/RootStore'
-import { createRouter, runRouter } from './router'
-import { routes } from './routes'
-import { App } from './components/App'
-
-import { api } from './api'
+import {api} from './api'
 
 // material-ui 需要這個東西
 injectTapEventPlugin()
 
 const stores = RootStoreModel.create({}, { api })
-
-const Redirect = Object.freeze({})
 
 const router = createRouter(routes)
 
