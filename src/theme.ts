424a3e1cf3316946d98bff323f93651bfde14178
ray851107 <ray851107@gmail.com>
2017-07-21 20:10:54 +0800

    add docs

--- src/theme.ts
+++ src/theme.ts
@@ -1,10 +1,13 @@
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import {} from 'material-ui/styles/colors'
 
+/**
+ * 自訂 material-ui 主題
+ * 更多選項可見：
+ * https://github.com/callemall/material-ui/blob/13548d80b9bc74a990262e6d3cd7e66df79950ea/src/styles/getMuiTheme.js#L3
+ * http://www.material-ui.com/#/customization/themes
+ */
 export const theme = getMuiTheme({
     palette: {
-    },
-    card: {
-        margin: 8
     }
 })
