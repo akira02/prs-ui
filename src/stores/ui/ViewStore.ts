5711fa22e6f9a90c22fc673416ec140a5d066e1d
ray851107 <ray851107@gmail.com>
2017-07-22 21:17:26 +0800

    keep old page for a while

--- src/stores/ui/ViewStore.ts
+++ src/stores/ui/ViewStore.ts
@@ -1,14 +1,35 @@
 import { types } from 'mobx-state-tree'
 import { PageDataModel, PageData } from './PageData'
 
+import * as shortId from 'shortid'
+
 export const ViewStoreModel = types.model(
     'ViewStore',
     {
-        page: types.maybe(PageDataModel)
+        pageMap: types.optional(types.map(PageDataModel), {}),
+        pageKey: types.maybe(types.string),
+        get page (): PageData | null {
+            if (this.pageKey == null) return null
+            return this.pageMap.get(this.pageKey)
+        }
     },
     {
         setPage(page: PageData) {
-            this.page = page
+            if (this.pageKey != null) {
+                this.sechduleDelete(this.pageKey)
+            }
+
+            const newKey = shortId.generate()
+            this.pageMap.set(newKey, page)
+            this.pageKey = newKey
+        },
+        sechduleDelete (key: string) {
+            setTimeout(() => {
+                this.deletePage(key)
+            }, 1000)
+        },
+        deletePage (key: string) {
+            this.pageMap.delete(key)
         }
     }
 )
